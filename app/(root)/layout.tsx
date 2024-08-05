24f5fee9b832a54bf4751f2d025e0a7af49c489b
Hareesh Kamble <105536023+Hareeshkamble@users.noreply.github.com>
2024-08-05 21:22:07 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main>
+            <h1>
+                SIDEBAR
+            </h1>
+            {children}
+        </main>
+    );
+}
